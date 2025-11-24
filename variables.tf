1b88811720c5c57a943556de501eb726b6df7e74
silentoxygen <suhvyb@gmail.com>
2025-11-24 10:40:19 +0300

    Refactor variable descriptions in variables.tf
    Updated variable descriptions to provide more clarity and context.

--- variables.tf
+++ variables.tf
@@ -1,48 +1,53 @@
-variable "name" {
-  description = "Name of the Docker container"
+variable "container_name" {
+  description = <<EOT
+Name of the Docker container.
+EOT
   type        = string
 }
 
 variable "image" {
-  description = "Docker image to run (e.g. nginx:latest)"
+  description = <<EOT
+Image to use for the container (e.g. nginx:latest).
+EOT
   type        = string
 }
 
 variable "internal_port" {
-  description = "Port inside the container that the app listens on"
+  description = <<EOT
+The internal port the container listens on.
+EOT
   type        = number
 }
 
 variable "external_port" {
-  description = "Port on the host to map to the internal port"
+  description = <<EOT
+The port exposed on the host system.
+EOT
   type        = number
 }
 
-variable "env" {
-  description = "Optional list of environment variables in KEY=VALUE form"
-  type        = list(string)
-  default     = []
-}
-
 variable "create_network" {
-  description = "Whether this module should create a Docker network"
+  description = <<EOT
+Whether to create a new Docker network.
+If false, you must supply 'network_name'.
+EOT
   type        = bool
   default     = true
 }
 
 variable "network_name" {
   description = <<EOT
-Name of the Docker network.
-
 If create_network = true, this is the name of the network to create.
 If create_network = false, this must be the name of an existing network.
 EOT
-  type    = string
-  default = "app_network"
+  type        = string
+  default     = "app_network"
 }
 
 variable "keep_image_locally" {
-  description = "Whether Docker should keep the image locally when the container is destroyed"
+  description = <<EOT
+If true, the image is not removed from the local Docker host.
+EOT
   type        = bool
   default     = false
 }
